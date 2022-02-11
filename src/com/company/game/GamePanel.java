d888c77a2d53bac8b47102afb2a0628f38eeef56
zd210349 <zack.duck@outlook.com>
2022-02-11 13:27:34 +0000

    fontSheet added

--- src/com/company/game/GamePanel.java
+++ src/com/company/game/GamePanel.java
@@ -124,7 +124,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     public void render() {
         if (g != null) {
-            g.setColor(new Color(66, 134,244));
+            g.setColor(new Color(0,255 ,0));
             g.fillRect(0,0, width, height);
             gsm.render(g);
         }
